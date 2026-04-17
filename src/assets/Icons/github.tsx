02618d7489c25e4db5117cd1ff7c9cd718cc664d
Aditi Saxena <adirak2708@gmail.com>
2026-04-17 23:21:18 +0530

    feat: add header and hero section for portfolio

---
+++ src/assets/Icons/github.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Github = () => {
+  return (
+    <img
+      src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAABaElEQVR4nN2VvUoDQRSFo1GITWInKe38ARHxEQQJEhQtxEpLn0LUgKKlpY2/pa2FnS8gCEGJYmGjiGjstNJPrkxguNzdmY2VHlgWdr9z7szd2Zlc7l8LyANTwB5wCbwCTeDKPZsTpt3wSeCGsO5kEFmCO4A14CsivCVht8UbU6BG+6qFwqXfvsaAKlA3wm7dNxhQzytJ4V2un7663bseYNTd5RoBSt5C8NVo+XSBGWOU/REt7TN8VQs8MlpQiChQMFbbvgXK2vY1Gwr3vNPKW7egZwUVMxQoKm/Tgl5+UaCkvB8WdKGg+QwFFpT33oJ2FfQADEaEDwGPyntigRUF/UwV2JAQgx8GNh2jtWgV6PT+2BXg1DPsGPwhtp7kZ0ya7gTwCbwDZeDMGcoJM7C0FOrpqgO3ApzMWOsgNdwzr7st+Bo4T+F8HZt7UODAaUjLUhgZxBuwHHUWGAF5YDzlvWzvvZmD/5S+AbIvgSRqpDGdAAAAAElFTkSuQmCC"
+      alt="github"
+    />
+  );
+};
+
+export default Github;
